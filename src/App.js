7b2bbcfdb70158e77a803646d9ef95ad598134cb
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-04-06 22:31:01 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/header/header'
+import Search from './components/search/search'
+import Footer from './components/footer/footer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <mainContentDiv/>;
 
 export default App;
