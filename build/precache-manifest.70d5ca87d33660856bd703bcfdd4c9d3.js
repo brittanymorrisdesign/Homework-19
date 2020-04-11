fef5584fc6bc33b199be118e3998155277902b15
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-04-11 14:15:36 -0400

    updated package.json

---
+++ build/precache-manifest.70d5ca87d33660856bd703bcfdd4c9d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d75f0fe831b8121514f2e668190073a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a55898f7b747eb6bff4",
+    "url": "/static/css/main.fe41fd5e.chunk.css"
+  },
+  {
+    "revision": "79b03f7f07a5cc5bfa9d",
+    "url": "/static/js/2.50072217.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50072217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a55898f7b747eb6bff4",
+    "url": "/static/js/main.0fbfac64.chunk.js"
+  },
+  {
+    "revision": "65ad947aa062314b9e3f",
+    "url": "/static/js/runtime-main.f8d9ea35.js"
+  }
+]);
